eafbddc93875b6186b06c657685fdb476c30317c
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-02 11:07:54 -0300

    feat: invalid enrollment

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,26 @@
+import styled from 'styled-components';
+import useEnrollment from '../../../hooks/api/useEnrollment';
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { enrollment } = useEnrollment();
+  return !enrollment ? (
+    <PaymentContainer>
+      Você precisa completar sua inscrição antes <br /> de prosseguir pra escolha de ingresso
+    </PaymentContainer>
+  ) : (
+    <PaymentContainer>incrição existe presencial ou online</PaymentContainer>
+  );
 }
+const PaymentContainer = styled.h1`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+  color: #8e8e8e;
+`;
