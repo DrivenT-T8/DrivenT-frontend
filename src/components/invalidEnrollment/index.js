30f62b6705fbe897cd8d0b26722f0d59522bf949
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-04 11:16:26 -0300

    fix: InvalidEnrollment component index

--- src/components/invalidEnrollment/index.js
+++ src/components/invalidEnrollment/index.js
@@ -1,8 +1,9 @@
+import TicketType from '../ticketType';
 import { PaymentContainer } from './invalidEnrollment';
 export default function InvalidEnrollment() {
   return (
-    <PaymentContainer>
-      Você precisa completar sua inscrição antes <br /> de prosseguipra escolha de ingresso
+    <PaymentContainer> {/*  Você precisa completar sua inscrição antes <br /> de prosseguir pra escolha de ingresso */}
+      <TicketType/>
     </PaymentContainer>
   );
-};
+}
