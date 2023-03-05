fe5dfac62895e70ceda67bc2c7f1308e2228a756
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-05 12:55:03 -0300

    feat: tickeType styled

---
+++ src/components/ticketType/ticketType.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+export const TicketTypeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 290px;
+  height: 108px;
+  left: 330px;
+  top: 292px;
+  background: #ffeed2;
+  border-radius: 20px;
+  margin: 10px 0;
+  h1 {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: center;
+    color: #454545;
+  }
+  p {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    text-align: center;
+    color: #898989;
+  }
+`;
