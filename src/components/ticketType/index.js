4198b79ca347aafdf70dbdfd42b3c0999346fa9a
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-05 15:54:22 -0300

    fix: settings

--- src/components/ticketType/index.js
+++ src/components/ticketType/index.js
@@ -2,14 +2,13 @@ import useTicket from '../../hooks/api/useTicket';
 import { TicketTypeContainer } from './ticketType';
 export default function TicketType() {
   const { ticket } = useTicket();
-  console.log(ticket.TicketType);
-  const tickeType = ticket.TicketType;
-  return tickeType ? (
+  console.log(ticket);
+  return ticket ? (
     <TicketTypeContainer>
-      {tickeType.isRemote ? (
+      {ticket.TicketType.isRemote ? (
         <div>
-          <h1>Presencial + {tickeType.includesHotel ? 'Com Hotel' : 'Sem Hotel'} </h1>
-          <p>R$ {tickeType.price}</p>
+          <h1>Presencial + {ticket.TicketType.includesHotel ? 'Com Hotel' : 'Sem Hotel'} </h1>
+          <p>R$ {ticket.TicketType.price}</p>
         </div>
       ) : (
         <h1>Online </h1>
