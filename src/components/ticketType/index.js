5e91b4113e5f5755a08056e86ebd9d6772665323
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-05 16:53:08 -0300

    fix: settings

--- src/components/ticketType/index.js
+++ src/components/ticketType/index.js
@@ -2,22 +2,23 @@ import useTicket from '../../hooks/api/useTicket';
 import { TicketTypeContainer } from './ticketType';
 export default function TicketType() {
   const { ticket } = useTicket();
-  console.log(ticket);
   return ticket ? (
     <TicketTypeContainer>
-      {!ticket.TicketType.isRemote ? (
+      {!ticket?.TicketType?.isRemote ? (
         <div>
-          <h1>Presencial + {ticket.TicketType.includesHotel ? 'Com Hotel' : 'Sem Hotel'} </h1>
-          <p>R$ {ticket.TicketType.price}</p>
+          <h1>Presencial + {ticket?.TicketType?.includesHotel ? 'Com Hotel' : 'Sem Hotel'} </h1>
+          <p>R$ {ticket?.TicketType?.price}</p>
         </div>
       ) : (
         <div>
           <h1>Online </h1>
-          <p>R$ {ticket.TicketType.price}</p>
+          <p>R$ {ticket?.TicketType?.price}</p>
         </div>
       )}
     </TicketTypeContainer>
   ) : (
-    <TicketTypeContainer> <h1> Escolha a Modalidade de Ingresso e Pagamento</h1></TicketTypeContainer>
+    <TicketTypeContainer>
+      <h1> Escolha a Modalidade de Ingresso e Pagamento</h1>
+    </TicketTypeContainer>
   );
 }
