b7add6af920a98306169d5e921bb1546e0eebafa
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-09 16:25:41 -0300

    fieat: booking context

--- src/contexts/TicketContext.js
+++ src/contexts/TicketContext.js
@@ -5,10 +5,12 @@ const TicketContext = createContext();
 export default TicketContext;
 
 export function TicketProvider({ children }) {
+  const [edit, setEdit] = useState(false);
+  const [bookingId, setBooking] = useState([]);
   const [typeTicketSelected, setTypeTicketSelected] = useState([]);
-  
+
   return (
-    <TicketContext.Provider value={{ typeTicketSelected, setTypeTicketSelected }}>
+    <TicketContext.Provider value={{ bookingId, setBooking, edit, setEdit, typeTicketSelected, setTypeTicketSelected }}>
       {children}
     </TicketContext.Provider>
   );
