5a87d4ca9decdb2d29293d508db2ee1e524f3551
Caio Beneton <caio.beneton@gmail.com>
2023-03-10 20:21:39 -0300

    feat: no hotel or payment check

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,5 +1,23 @@
 import HotelComponents from '../../../components/HotelComponents';
+import HotelNotIncluded from '../../../components/HotelComponents/invalidTicket/hotelNotIncluded';
+import PaymentRequired from '../../../components/HotelComponents/invalidTicket/paymentRequired';
+import useTicket from '../../../hooks/api/useTicket';
 
 export default function Hotel() {
+  const { ticket } = useTicket();
+
+  const includesHotel = ticket?.TicketType.includesHotel;
+  const ticketStatus = ticket?.ticketStatus;
+
+  console.log(ticket);
+
+  if (ticketStatus !== 'PAID' || !ticket) {
+    return <PaymentRequired />;
+  }
+
+  if (!includesHotel) {
+    return <HotelNotIncluded />;
+  }
+
   return <HotelComponents />;
 }
