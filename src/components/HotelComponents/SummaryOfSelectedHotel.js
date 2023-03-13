cb4a521254ad1f0c160d2d2f5965e46372ff7c41
Josana-Kla <josana.0205@gmail.com>
2023-03-13 13:26:54 -0300

    feat: showed name, image of hotel and accomodation type dynamically

--- src/components/HotelComponents/SummaryOfSelectedHotel.js
+++ src/components/HotelComponents/SummaryOfSelectedHotel.js
@@ -1,18 +1,51 @@
 import { ButtonHotel } from './ButtonHotel';
+import useUserBooking from '../../hooks/api/useUserBooking';
+import useHotel from '../../hooks/api/useHotel';
 
 export default function SummaryOfSelectedHotel() {
+  const { booking } = useUserBooking();
+  const { hotel } = useHotel();
+
+  function getHotelById() {
+    const hotelIdInsideRoomOfBooking = booking?.Room?.hotelId;
+    let imageHotelSelected;
+    let nameHotelSelected;
+    
+    hotel?.forEach(e => {
+      if(e.id === hotelIdInsideRoomOfBooking) {
+        imageHotelSelected = e.image;
+        nameHotelSelected = e.name;
+      }
+    });
+
+    return {
+      imageHotelSelected,
+      nameHotelSelected
+    };
+  }
+
+  function showAccommodationType() {
+    if (booking?.Room?.capacity === 1) { 
+      return 'Single'; 
+    } else if (booking?.Room?.capacity === 2) { 
+      return 'Double'; 
+    } else if (booking?.Room?.capacity === 3) { 
+      return 'Triple'; 
+    }
+  }
+
   return(
     <>
       <ButtonHotel isColorButtonSelected={true} >
-        <img src='https://www.melhoresdestinos.com.br/wp-content/uploads/2021/04/resort-salinas-maragogi-capa-05.jpg' alt="hotel" />
-        <h3>Driven Resort</h3>
+        <img src={getHotelById().imageHotelSelected} alt="hotel" />
+        <h3>{getHotelById().nameHotelSelected}</h3>
         <span>
           <p>Quarto reservado:</p>
-          <p>101 (Triple)</p>
+          <p>{booking?.Room?.name} ({showAccommodationType()})</p>
         </span>
         <span>
-          <p>Pessoas no seu quarto</p>
-          <p>Você e mais 2</p>
+          <p>Pessoas no seu quarto:</p>
+          <p>Você e mais {booking?.Room?.capacity - 1}</p>
         </span>
       </ButtonHotel>
     </>
